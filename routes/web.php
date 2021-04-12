00df4f3d06ea0f23536a1e9751e046cae257afa6
aryaseptiawan <aryaseptiawan18@gmail.com>
2021-04-12 20:32:34 +0700

    Latihan Prak 9 Fix

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/mahasiswa');
 });
+
+Route::resource('/mahasiswa', MahasiswaController::class);
+Route::get('mahasiswa/nilai/{nim}', [MahasiswaController::class, 'show_khs'])->name('mahasiswa.khs');
